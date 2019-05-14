0d832f6c0bace3dcefc1d03cb680f404638f1fc2
omar <omar.ihab.12@gmail.com>
2019-05-14 14:33:30 +0200

    - add product table to database - create product controller - finish creating new product page view and functionality with uploading image functionality (Product/Create) - finsh product list page (product/Index)

---
+++ SmarttechTask/Controllers/ProductsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using SmarttechTask.Models;
+
+namespace SmarttechTask.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(string productString, HttpPostedFileBase photo)
+        {
+            Product product = JsonConvert.DeserializeObject<Product>(productString);
+
+            if(photo != null)
+            {
+                product.Photo = new byte[photo.ContentLength];
+                photo.InputStream.Read(product.Photo, 0, photo.ContentLength);
+            }
+            product.Id = (db.Products.Count() > 0 ? db.Products.Max(p => p.Id) : 0) + 1;
+
+            product.LastUpdate = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(/*product*/);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Photo,Price,LastUpdate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SmarttechTask/Scripts/Products/create.js
@@ -0,0 +1,61 @@
+﻿
+$(document).ready(function () {
+
+    $("#productImageSelector").change(function () {
+        ChangeDisplayedImage(this);
+    });
+
+    $("#createProduct").click(function () {
+        CreateProduct();
+    });
+
+});
+
+function ChangeDisplayedImage(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#productImage').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function CreateProduct() {
+
+    if ($.trim($("#Name").val()) == "") {
+        $("#Name").siblings(".field-validation-valid").html("The Name field is required.");
+    }
+    else if (parseFloat($("#Price").val()) == null) {
+        //$("#Price").siblings(".field-validation-valid").html("The Price should be number");
+    }
+    else {
+        var product =
+        {
+            "Name": $("#Name").val(),
+            "Price": parseFloat($("#Price").val())
+        };
+
+        var photo = document.getElementById('productImageSelector').files[0];
+
+        sendData = new FormData();
+        sendData.append("productString", JSON.stringify(product));
+        sendData.append("photo", photo);
+
+        $.ajax({
+            url: $("#Create").attr("action"),
+            method: "POST",
+            processData: false,
+            contentType: false,
+            data: sendData,
+            success: function (result) {
+                alert("Product added successfully!");
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log(JSON.stringify(xhr) + " " + ajaxOptions + ": " + thrownError);
+                alert("Product wasn't added");
+            }
+        });
+    }
+}
